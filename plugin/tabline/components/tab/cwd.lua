b7a4c5ba34805218d64c05e8f592f45d5b756894
Myroslav Rys <stonevil@gmail.com>
2025-01-25 00:47:17 -0800

    TABLINE: do not trim cwd if max_length is 0

--- plugin/tabline/components/tab/cwd.lua
+++ plugin/tabline/components/tab/cwd.lua
@@ -7,8 +7,10 @@ return {
     if cwd_uri then
       local file_path = cwd_uri.file_path
       cwd = file_path:match('([^/]+)/?$')
-      if cwd and #cwd > opts.max_length then
-        cwd = cwd:sub(1, opts.max_length - 1) .. '…'
+      if opts.max_length ~= 0 then
+        if cwd and #cwd > opts.max_length then
+          cwd = cwd:sub(1, opts.max_length - 1) .. '…'
+        end
       end
     end
     return cwd or ''
