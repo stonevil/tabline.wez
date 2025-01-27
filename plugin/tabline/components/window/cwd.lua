4503a136bffb771a57dd519d22291871c78b5eb2
Myroslav Rys <stonevil@gmail.com>
2025-01-26 20:45:17 -0800

    TABLINE: change var name, fix function, etc

--- plugin/tabline/components/window/cwd.lua
+++ plugin/tabline/components/window/cwd.lua
@@ -1,18 +1,17 @@
-local apcwd = ''
+local cwd = ''
 
 return {
   default_opts = { max_length = 0 },
-  update = function(tab, opts)
-    local apcwd_uri = tab.active_pane.current_working_dir
-    if apcwd_uri then
-      local file_path = apcwd_uri.file_path
-      apcwd = file_path:match('([^/]+)/?$')
+  update = function(window, opts)
+    local cwd_uri = window:active_pane():get_current_working_dir()
+    if cwd_uri then
+      cwd = cwd_uri.file_path
       if opts.max_length ~= 0 then
-        if apcwd and #apcwd > opts.max_length then
-          apcwd = apcwd:sub(1, opts.max_length - 1) .. '…'
+        if cwd and #cwd > opts.max_length then
+          cwd = cwd:sub(1, opts.max_length - 1) .. '…'
         end
       end
     end
-    return apcwd or ''
+    return cwd or ''
   end,
 }
