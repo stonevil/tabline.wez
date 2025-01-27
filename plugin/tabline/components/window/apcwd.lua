417865533b0c51bb08f19aa34900ed2bd9338eec
Myroslav Rys <stonevil@gmail.com>
2025-01-26 19:15:09 -0800

    TABLINE: rename var

--- plugin/tabline/components/window/apcwd.lua
+++ plugin/tabline/components/window/apcwd.lua
@@ -1,18 +1,18 @@
-local cwd = ''
+local apcwd = ''
 
 return {
   default_opts = { max_length = 0 },
   update = function(tab, opts)
-    local cwd_uri = tab.active_pane.current_working_dir
-    if cwd_uri then
-      local file_path = cwd_uri.file_path
-      cwd = file_path:match('([^/]+)/?$')
+    local apcwd_uri = tab.active_pane.current_working_dir
+    if apcwd_uri then
+      local file_path = apcwd_uri.file_path
+      apcwd = file_path:match('([^/]+)/?$')
       if opts.max_length ~= 0 then
-        if cwd and #cwd > opts.max_length then
-          cwd = cwd:sub(1, opts.max_length - 1) .. '…'
+        if apcwd and #apcwd > opts.max_length then
+          apcwd = apcwd:sub(1, opts.max_length - 1) .. '…'
         end
       end
     end
-    return cwd or ''
+    return apcwd or ''
   end,
 }
